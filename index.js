58f75dd0bbf5544849719bfdf0fa1a30d868b637
Daniel Ross <daniel@danielross.org>
2015-07-23 18:26:20 -0700

    Make it listen to the right port.

--- index.js
+++ index.js
@@ -29,9 +29,7 @@ app.get('/fudge_dice', function(req, res) {
   res.send(dice);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
 });
