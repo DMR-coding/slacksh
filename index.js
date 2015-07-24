b32f8138a1167c53d41d9e9ab85fc396789907e6
Daniel Ross <daniel@danielross.org>
2015-07-23 18:17:11 -0700

    FUDGE die roller

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+function roll() {
+  var num = Math.floor(Math.random() * 3 - 1);
+  var die;
+
+  if ( num === - 1 ){
+    die = ":minus:";
+  }else if ( num === 1 ){
+    die = ":plus:";
+  }else {
+    die = ":blank:";
+  }
+
+  return {die: die, num: num};
+}
+
+
+app.get('/fudge_dice', function(req, res) {
+  var dice = "", result, sum = 0;
+  for ( var i = 0; i < 4; i++ ){
+    result = roll();
+    dice += result.die;
+    sum += result.num;
+  }
+
+  dice += " (" + sum + ")";
+  res.send(dice);
 });
 
 var server = app.listen(3000, function () {
